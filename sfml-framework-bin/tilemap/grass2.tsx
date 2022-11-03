0df8665a7beba7acd1920621b1fbdd33149d757a
Librarian423 <youg4231998@naver.com>
2022-11-03 09:55:43 +0900

    clean up resources

--- sfml-framework-bin/tilemap/grass2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="grass2" tilewidth="32" tileheight="32" tilecount="18" columns="3">
- <image source="plains.png" width="96" height="192"/>
-</tileset>
